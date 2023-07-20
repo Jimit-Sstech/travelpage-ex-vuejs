a8ae40ef296942e0048e6df7ad6815fe7a78044f
Jimit-Sstech <Jimit.sstech@gmail.com>
2023-07-20 17:01:13 +0530

    Initialize the project.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <navbar-view />
+  <router-view />
 </template>
+
+<script>
+import NavbarView from './components/NavbarView.vue';
+export default {
+  components: {
+    NavbarView
+  },
+};
+</script>
 
 <style>
 #app {
